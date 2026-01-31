582e847bffe1135d2c4b1142bc52bff6f97788c5
JoyIsHappii <chayaninkosol@gmail.com>
2026-01-30 16:02:33 -0800

    excercise 24 completed

--- exercises/ex24.js
+++ exercises/ex24.js
@@ -21,7 +21,26 @@ console.log(lastIndexOf([], 3), "=?", -1);
 
 */
 
-const myArray = [0, 1, 4, 1, 2 ];
-for (let i=0; i<myIndex.length; i++) {
-    
+const lastIndexOf = function (myArray,value) {
+    let result = -1;
+    for (let i=myArray.length-1 ; i >=0; i--) {
+        if (myArray[i] === value) {
+         return i;
+    }
+    }
+return result;
 }
+
+/* console.log (lastIndexOf([0, 1, 4, 1, 2 ], 1));//3
+console.log (lastIndexOf([ 0, 1, 4, 1, 2 ], 2));//4
+console.log (lastIndexOf([ 0, 1, 4, 1, 2 ], 3));//-1
+console.log (lastIndexOf([ 5, 5, 5 ], 5));//2
+console.log (lastIndexOf([], 3));//-1 */
+
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 1), "=?", 3);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 2), "=?", 4);
+console.log(lastIndexOf([ 0, 1, 4, 1, 2 ], 3), "=?", -1);
+console.log(lastIndexOf([ 5, 5, 5 ], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+
+//done
